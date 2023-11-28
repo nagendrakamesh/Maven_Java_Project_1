874d2435a0cc5972079e1ff6e2e137f1a8a93b93
nagendrakamesh <kameshoncall@gmail.com>
2023-11-28 14:33:13 +0530

    first commit

---
+++ src/main/java/com/app/demo/Java_Maven_Project_1/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.Java_Maven_Project_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Hemanth!" );
+    }
+}
